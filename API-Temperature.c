fc58ade1f5535d5b2bdd84db1997f3c17a60198f
AbhiYellapu <158604163+AbhiYellapu@users.noreply.github.com>
2024-02-05 11:12:26 +0530

    Update API-Temperature.c

--- API-Temperature.c
+++ API-Temperature.c
@@ -1,25 +1,29 @@
-// Testing system()
+// Find the temperature of the given city using Weather-API
 
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
 #define MAXCITYNAMELENGTH 20
+#define MAXAPICOMMANDLENGTH 200
+#define MAXAPILENGTH 150
 #define DATAFILE "weather.txt"
 
 void main()
 {
-	char api[150];
+	char api[MAXAPILENGTH];
 	char city[MAXCITYNAMELENGTH];
-	char apiCommand[200];
+	char apiCommand[MAXAPICOMMANDLENGTH];
 	FILE *fpApi;
 	char *temperature;
 	bool isCityFound = false;
 	printf("Enter the city name to find the temperature: ");
 	scanf("%s", city);
+
 	sprintf(api, "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=abe3a0f4d0b6cebfbe7393b4b4e3aa28&units=metric", city);
 	sprintf(apiCommand, "curl \"%s\" >%s", api, DATAFILE);
 	system(apiCommand);
+	
 	fpApi = fopen(DATAFILE, "r");
 	fseek(fpApi, 0, SEEK_END);
 	long int apiSize = ftell(fpApi);
