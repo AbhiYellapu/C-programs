8f78be07e60d4080f94d444c89aa91bc5b85777f
AbhiYellapu <158604163+AbhiYellapu@users.noreply.github.com>
2024-02-05 11:18:50 +0530

    Update API-Temperature.c

--- API-Temperature.c
+++ API-Temperature.c
@@ -21,7 +21,7 @@ void main()
 	scanf("%s", city);
 
 	sprintf(api, "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=abe3a0f4d0b6cebfbe7393b4b4e3aa28&units=metric", city);
-	sprintf(apiCommand, "curl \"%s\" >%s", api, DATAFILE);
+	sprintf(apiCommand, "curl -s \"%s\" >%s", api, DATAFILE);
 	system(apiCommand);
 	
 	fpApi = fopen(DATAFILE, "r");
@@ -33,17 +33,18 @@ void main()
 	apiData[apiSize] = '\0';
 	fclose(fpApi);
 
-	temperature = strtok(apiData, "{}[],\":");
+	char delimiters[] = "{}[],\":";
+	temperature = strtok(apiData, delimiters);
     while (temperature != NULL) 
     {
         if (strcmp(temperature, "temp") == 0) 
         {
-            temperature = strtok(NULL, "{}[],\":");
+            temperature = strtok(NULL, delimiters);
             printf("Temperature in %s: %s\n", city, temperature);
             isCityFound = true;
             break;
         }
-        temperature = strtok(NULL, "{}[],\":");
+        temperature = strtok(NULL, delimiters);
     }
     if(!isCityFound)   
     {
