f4841cb0e63554512e2cdc6b33f14841bf4dbfe1
AbhiYellapu <158604163+AbhiYellapu@users.noreply.github.com>
2024-02-19 14:56:47 +0530

    Update TestPrime.c

--- TestPrime.c
+++ TestPrime.c
@@ -7,15 +7,15 @@ void testPrime()
 {
 	int counter;
 	int passStatus = 0;
-	char *numbers[] = {"2", "3", "5", "37", "41", "11", "47", "ak", "z", "-1", "4", "0", "!", "\t", " \n"};
-	int expectedResult[] = {1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0};
-	int countOfNumbers = 15 ;
+	char *numbers[] = {"2", "3", "5", "37", "41", "11", "47", "ak", "z", "-1", "4", "0", "!", "\t", "\n", " ", "prime2"};
+	int expectedResult[] = {1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int countOfNumbers = 17 ;
 
 	for  (counter = 0; counter < countOfNumbers; counter++)
 	{
 		if (isPrime(numbers[counter]) != expectedResult[counter])
 		{
-			printf("failed(%s)\n", numbers[counter]);
+			printf("failed('%s')\n", numbers[counter]);
 			passStatus++;
 		}
 	}
